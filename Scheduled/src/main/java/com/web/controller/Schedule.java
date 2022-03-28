27c04c69d83d68b84aa4376d96705adcbf0caf37
kwonseeun <0115km@naver.com>
2022-03-28 16:48:54 +0900

    @Scheduled 로직 추가

--- Scheduled/src/main/java/com/web/controller/Schedule.java
+++ Scheduled/src/main/java/com/web/controller/Schedule.java
@@ -1,5 +1,17 @@
 package com.web.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Schedule {
 
+	@Scheduled(cron = "0 0/1 * * * ?")
+	public void cronRun() {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println("### Cron Run 1 Minutes = " + simpleDateFormat.format(new Date()));
+	}
 }
